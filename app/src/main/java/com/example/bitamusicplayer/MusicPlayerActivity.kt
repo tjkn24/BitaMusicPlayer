4fe0476dd5094674c2219e3606a07d3f77de433c
tjkn24 <tjkn24@gmail.com>
2021-08-19 15:18:25 +0700

    Before adding controls to card view

--- app/src/main/java/com/example/bitamusicplayer/MusicPlayerActivity.kt
+++ app/src/main/java/com/example/bitamusicplayer/MusicPlayerActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bitamusicplayer
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,12 @@ class MusicPlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_player)
+
+        if (Build.VERSION.SDK_INT >= 23)
+            checkPermission()
+    }
+
+    private fun checkPermission() {
+
     }
 }
